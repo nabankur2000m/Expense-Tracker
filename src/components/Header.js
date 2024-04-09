786e27e0f66e172a5a4d5763ce25d04b1c7d0c96
NABANKUR MONDAL <76182696+nabankur2000m@users.noreply.github.com>
2024-04-09 12:56:07 +0530

    Creating Signup Screen

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/products">Products</a></li>
+                    <li><a href="/about-us">About Us</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
